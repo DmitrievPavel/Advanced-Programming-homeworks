33121ec11aa91426de737f5a4a350424e61284b6
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-01-26 19:49:58 +0300

    Add files via upload

---
+++ RAII/task1/task1.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <iostream>
+
+class smart_array {
+
+private:
+    int* ptr;
+    int size_;
+    int count;
+
+public:
+    smart_array(int size) {
+        size_ = size;
+        ptr = new int[size];
+        count = 0;
+    }
+
+    ~smart_array() {
+        delete[] ptr;
+    }
+
+    void add_element(int a) {
+        if (count >= size_) {
+            throw std::length_error("Oversize");
+        }
+        ptr[count] = a;
+        count++;
+    }
+
+    int get_element(int i) {
+        if (i < 0 || i >= count) {
+            throw std::out_of_range("Incorrect_index");
+        }
+        return ptr[i];
+    }
+};
+
+int main()
+{
+    try {
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+       // arr.add_element(10);
+        std::cout << arr.get_element(1) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+}
+
