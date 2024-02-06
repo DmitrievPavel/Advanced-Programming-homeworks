1fd80f3034166e27bd55b77adf3c76b5a9479068
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-06 21:51:15 +0300

    Add files via upload

---
+++ Lesson4/task1/task1.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+#include <vector>
+
+template <class T>
+
+T pow(T a) {
+    return a * a;
+}
+
+template <>
+
+std::vector<int> pow(std::vector<int> a) {
+
+    for (int i = 0; i < a.size(); ++i) {
+        a[i] *= a[i];
+    }
+
+    return a;
+}
+
+int main()
+{
+    int a = 4;
+
+    std::cout << "[IN] " << a << "\n[OUT] " << pow(a) << std::endl;
+
+    std::vector<int> vec = { -1, 4, 8 };
+
+    std::cout << "[IN] ";
+
+    for (int i : vec) {
+        std::cout << i << " ";
+    }
+
+    std::cout << "\n[OUT] ";
+
+    for (int i : pow(vec)) {
+        std::cout << i << " ";
+    }
+
+}
+
