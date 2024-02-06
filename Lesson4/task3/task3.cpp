7f3c1792b6042cc95b4c0b4b539da1e4ec715ade
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-06 21:52:15 +0300

    Add files via upload

---
+++ Lesson4/task3/task3.cpp
@@ -0,0 +1,48 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+class counter {
+private:
+    std::vector<int> vec;
+    int sum;
+    int count;
+public:
+    counter(std::vector<int> a) {
+        vec = a;
+        sum = 0;
+        count = 0;
+    }
+    void operator()() {
+        for (auto i : vec) {
+            sum += i;
+            if (i % 3 == 0) {
+                count++;
+            }
+        }
+    }
+    int get_sum() {
+        return sum;
+    }
+    int get_count() {
+        return count;
+    }
+    void print() {
+        for (auto i : vec) {
+            std::cout << i << " ";
+        }
+    }
+};
+
+int main()
+{
+    std::vector<int> v = { 4, 1, 3, 6, 25, 54 };
+    counter c(v);
+    c.print();
+    c();
+    std::cout << "\nSum: " << c.get_sum() << "\nCount: " << c.get_count();
+
+}
+
+
