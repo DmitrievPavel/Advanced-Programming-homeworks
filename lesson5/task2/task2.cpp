ceb39b8bbd81b8da65ab63e35cc10181c5e7c52f
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-09 12:24:55 +0300

    Add files via upload

---
+++ lesson5/task2/task2.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <set>
+
+int main()
+{
+    int count = 0;
+    int size = 0;
+    std::cout << "input size: ";
+    std::cin >> size;
+    std::set <int, std::greater <int>> myset;
+
+    while (count < size) {
+        int x = 0;
+        std::cout << "input elem: ";
+        std::cin >> x;
+        myset.insert(x);
+        count++;
+    }
+
+    std::cout << "output elem: \n";
+    for (const auto& i : myset) {
+        std::cout << i << std::endl;
+    }
+}
+
+
