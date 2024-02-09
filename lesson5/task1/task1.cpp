3e990bf33186b960ebf0b8e9a26877a2eb6a17b4
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-09 12:24:11 +0300

    Add files via upload

---
+++ lesson5/task1/task1.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+bool sor(const std::pair<char, int>& a, const std::pair <char, int>& b) {
+    return a.second > b.second;
+}
+
+int main()
+{
+    std::string s = { "Hello World!!" };
+    std::map<char, int> m;
+    for (char i : s) {
+        ++m[i];
+    }
+    std::vector <std::pair<char, int>> vec(m.begin(), m.end());
+    std::sort(vec.begin(), vec.end(), sor);
+    for (const auto& i : vec) {
+        std::cout << i.first << ":" << i.second << std::endl;
+    }
+
+}
