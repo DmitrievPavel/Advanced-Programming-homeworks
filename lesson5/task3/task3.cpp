8fa6e82f2cfb15854446da138efde6b3539dc0fe
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-09 14:27:37 +0300

    Add files via upload

--- lesson5/task3/task3.cpp
+++ lesson5/task3/task3.cpp
@@ -17,8 +17,36 @@ public:
         ptr = new T[logic_size];
     }
 
+    Myvector(const Myvector& right) {
+
+        T* tmp = new T[right.logic_size];
+
+        for (int i = 0; i < right.actual_size; ++i) {
+            tmp[i] = right.ptr[i];
+        }
+
+        delete[] ptr;
+        logic_size = right.logic_size;
+        actual_size = right.actual_size;
+        ptr = tmp;
+    }
+
     ~Myvector() {
         delete[] ptr;
+    }
+
+    Myvector& operator=(const Myvector& right) {
+        T* tmp = new T[right.logic_size];
+
+        for (int i = 0; i < right.actual_size; ++i) {
+            tmp[i] = right.ptr[i];
+        }
+
+        delete[] ptr;
+        logic_size = right.logic_size;
+        actual_size = right.actual_size;
+        ptr = tmp;
+        return *this;
     }
 
     T at(size_t i) {
@@ -29,12 +57,15 @@ public:
     }
 
     void push_back(T value) {
+
         if (actual_size == logic_size) {
             logic_size *= 2;
             T* tmp = new T[logic_size];
+
             for (int i = 0; i < actual_size; ++i) {
                 tmp[i] = ptr[i];
             }
+
             delete[] ptr;
             ptr = tmp;
         }
@@ -59,7 +90,23 @@ int main()
     vec.push_back(1);
     vec.push_back(2);
     vec.push_back(0.25);
+
     std::cout << "size: " << vec.size() << " capacity: " << vec.capacity() << std::endl;
+    for (int i = 0; i < vec.size(); ++i) {
+        std::cout << vec.at(i) << " ";
+    }
+
+    Myvector<double> vec2(vec);
+    vec2.push_back(9);
+
+    std::cout << "\nsize: " << vec2.size() << " capacity: " << vec2.capacity() << std::endl;
+    for (int i = 0; i < vec2.size(); ++i) {
+        std::cout << vec2.at(i) << " ";
+    }
+
+    vec = vec2;
+
+    std::cout << "\nsize: " << vec.size() << " capacity: " << vec.capacity() << std::endl;
     for (int i = 0; i < vec.size(); ++i) {
         std::cout << vec.at(i) << " ";
     }
