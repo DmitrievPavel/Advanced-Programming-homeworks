b7aae36d6da4e91e8e80441fca3f1e9c4542f420
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-10 20:56:19 +0300

    Add files via upload

---
+++ lesson6/task2/task2.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template <class T>
+void print(const T& cont) {
+
+    for (auto it = cont.begin(); it != cont.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print(test_set);
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print(test_list);
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print(test_vector);
+
+}
+
+
