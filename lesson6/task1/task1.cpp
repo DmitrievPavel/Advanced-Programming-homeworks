b36f48bd9f8bc62567ad1389e52632e57c39380b
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-10 20:55:45 +0300

    Add files via upload

---
+++ lesson6/task1/task1.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <class T>
+void unique(std::vector<T>& vec) {
+
+    std::sort(vec.begin(), vec.end());
+    auto it = std::unique(vec.begin(), vec.end());
+    vec.erase(it, vec.end());
+
+}
+
+int main()
+{
+    std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    for (const auto& i : vec) {
+        std::cout << i << " ";
+    }
+    unique(vec);
+    std::cout << std::endl;
+    for (const auto& i : vec) {
+        std::cout << i << " ";
+    }
+}
+
