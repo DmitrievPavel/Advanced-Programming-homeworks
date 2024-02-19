31d8e5bd35cf7178f2e2c85dc3a8425e75557b45
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2024-02-19 21:06:42 +0300

    Add files via upload

---
+++ lesson8/task1/task1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+void move_vectors(std::vector<T>& l, std::vector<T>& r) {
+    r = std::move(l);
+}
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    std::cout << "one: ";
+    for (auto& i : one) {
+        std::cout << i << "\t";
+    }
+    std::cout << "\ntwo: ";
+    for (auto& i : two) {
+        std::cout << i << "\t";
+    }
+
+    move_vectors(one, two);
+
+    std::cout << "\none: ";
+    for (auto& i : one) {
+        std::cout << i << "\t";
+    }
+    std::cout << "\ntwo: ";
+    for (auto& i : two) {
+        std::cout << i << "\t";
+    }
+}
+
